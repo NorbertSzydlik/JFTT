61edf9037884fa8530e3e92f139494429c53b5a0
Norbert Szydlik <kefaise@gmail.com>
2017-01-28 11:58:49 +0000

    Add new test for array assignment

--- tests/test_Assignment.cpp
+++ tests/test_Assignment.cpp
@@ -26,7 +26,7 @@ public:
   }
 };
 
-TEST_CASE("AssignmentTest", "simple")
+TEST_CASE("simple", "AssignmentTest")
 {
   std::string program = R"DELIM(
 VAR
@@ -48,4 +48,39 @@ BEGIN
 
   REQUIRE(result != 0);
   REQUIRE(programOutput.str() == "1\n");
+}
+
+TEST_CASE("array", "AssignmentTest")
+{
+  std::string program = R"DELIM(
+VAR
+  a[100]
+BEGIN
+  a[0] := 1;
+  WRITE a[0];
+  a[5] := 15;
+  WRITE a[5];
+  END
+)DELIM";
+
+  auto compiled = AssignmentTest().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  //std::ostream& out, std::istream& programStdIn, std::ostream& programStdOut
+  std::ostringstream programOutput;
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+
+  REQUIRE(result != 0);
+
+  std::istringstream programOutputStream(programOutput.str());
+
+  int outputNumber;
+
+  programOutputStream >> outputNumber;
+  REQUIRE(outputNumber == 1);
+
+  programOutputStream >> outputNumber;
+  REQUIRE(outputNumber == 15);
 }
