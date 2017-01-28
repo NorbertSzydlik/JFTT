27b12f688128c3201eb846d3fbe79b59bcc884e0
Norbert Szydlik <kefaise@gmail.com>
2017-01-28 22:15:08 +0000

    Add more tests to while

--- tests/test_While.cpp
+++ tests/test_While.cpp
@@ -7,7 +7,7 @@
 
 #include "logging.h"
 
-TEST_CASE("simple increment", "While")
+TEST_CASE("simple increment operator <", "While")
 {
   std::string program = R"DELIM(
 VAR
@@ -34,4 +34,96 @@ END
 
   REQUIRE(result > 0);
   Fixture().checkOutput(programOutput, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+}
+
+TEST_CASE("simple increment operator !=", "While")
+{
+  std::string program = R"DELIM(
+VAR
+  a
+BEGIN
+  a := 1;
+  WHILE a != 10 DO
+    WRITE a;
+    a := a + 1;
+  ENDWHILE
+END
+)DELIM";
+
+  auto compiled = Fixture().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  //std::ostream& out, std::istream& programStdIn, std::ostream& programStdOut
+  std::ostringstream programOutput;
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+
+  LOG("program out:\n" << programOutput.str() << "=====");
+
+  REQUIRE(result > 0);
+  Fixture().checkOutput(programOutput, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+}
+
+TEST_CASE("simple increment operator =", "While")
+{
+  std::string program = R"DELIM(
+VAR
+  a
+BEGIN
+  a := 1;
+  WHILE a = 1 DO
+    WRITE a;
+    a := a + 1;
+  ENDWHILE
+
+  WHILE a = 3 DO
+    WRITE a;
+    a := a + 1;
+  ENDWHILE
+END
+)DELIM";
+
+  auto compiled = Fixture().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  //std::ostream& out, std::istream& programStdIn, std::ostream& programStdOut
+  std::ostringstream programOutput;
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+
+  LOG("program out:\n" << programOutput.str() << "=====");
+
+  REQUIRE(result > 0);
+  Fixture().checkOutput(programOutput, {1});
+}
+
+TEST_CASE("simple increment operator <=", "While")
+{
+  std::string program = R"DELIM(
+VAR
+  a
+BEGIN
+  a := 1;
+  WHILE a <= 10 DO
+    WRITE a;
+    a := a + 1;
+  ENDWHILE
+END
+)DELIM";
+
+  auto compiled = Fixture().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  //std::ostream& out, std::istream& programStdIn, std::ostream& programStdOut
+  std::ostringstream programOutput;
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+
+  LOG("program out:\n" << programOutput.str() << "=====");
+
+  REQUIRE(result > 0);
+  Fixture().checkOutput(programOutput, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 }
