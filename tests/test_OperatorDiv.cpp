22cfbe4334c877d2305b5b7f46705121666d32ea
Norbert Szydlik <kefaise@gmail.com>
2017-01-29 14:22:12 +0000

    Add more tests for div

--- tests/test_OperatorDiv.cpp
+++ tests/test_OperatorDiv.cpp
@@ -31,7 +31,7 @@ BEGIN
   Fixture().checkOutput(programOutput, {3});
 }
 
-TEST_CASE("div identifiers", "OperatorAdd")
+TEST_CASE("div both identifiers", "OperatorAdd")
 {
   std::string program = R"DELIM(
 VAR
@@ -42,6 +42,72 @@ BEGIN
   c := a / b;
   WRITE c;
   d := a % b;
+  WRITE d;
+END
+)DELIM";
+
+  auto compiled = Fixture().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  std::ostringstream programOutput;
+
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+  LOG(programOutput.str());
+
+  REQUIRE(result > 0);
+
+  Fixture().checkOutput(programOutput, {
+    51 / 12,
+    51 % 12
+  });
+}
+
+TEST_CASE("div left identifiers", "OperatorAdd")
+{
+  std::string program = R"DELIM(
+VAR
+  a b c d
+BEGIN
+  a := 51;
+  b := 12;
+  c := a / 12;
+  WRITE c;
+  d := a % 12;
+  WRITE d;
+END
+)DELIM";
+
+  auto compiled = Fixture().compile(program);
+
+  std::istringstream compiledFile(compiled);
+  std::ostringstream stdOut;
+  std::istringstream stdIn;
+  std::ostringstream programOutput;
+
+  auto result = interpret(compiledFile, stdOut, stdIn, programOutput);
+  LOG(programOutput.str());
+
+  REQUIRE(result > 0);
+
+  Fixture().checkOutput(programOutput, {
+    51 / 12,
+    51 % 12
+  });
+}
+
+TEST_CASE("div right identifiers", "OperatorAdd")
+{
+  std::string program = R"DELIM(
+VAR
+  a b c d
+BEGIN
+  a := 51;
+  b := 12;
+  c := 51 / 12;
+  WRITE c;
+  d := 51 % 12;
   WRITE d;
 END
 )DELIM";
